3f0d013ae562d3379d277d9cf47d04286333ec82
alainlemus <alainttlm@gmail.com>
2026-05-07 16:27:45 -0600

    Add view transitions for page navigation

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="view-transition" content="same-origin">
     <title>{{ $title ?? 'Funeraria García de Bolívar' }}</title>
     @if(isset($siteInfo) && $siteInfo && $siteInfo->favicon)
     <link rel="icon" href="{{ asset('storage/' . $siteInfo->favicon) }}" type="image/x-icon" />
