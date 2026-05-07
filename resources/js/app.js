045a8d5543a7fa465708a411aa44ac74410385e5
alainlemus <alainttlm@gmail.com>
2026-05-07 16:30:12 -0600

    Add view transition support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+/* View Transitions - Livewire handles the hard part, we just trigger it */
+if (document.startViewTransition) {
+    let isTransitioning = false;
+
+    document.addEventListener('livewire:navigating', () => {
+        if (!isTransitioning) {
+            isTransitioning = true;
+            document.startViewTransition(() => {
+                isTransitioning = false;
+            });
+        }
+    });
+}
